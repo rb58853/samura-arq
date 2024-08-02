d5b9fdfdbda8b34ead5d26a37b5dc7f40affc20f
rb5883 <rb58853@gmail.com>
2024-08-02 12:18:46 -0400

    save

--- src/components/bars/bottomBar/bottomBar.jsx
+++ src/components/bars/bottomBar/bottomBar.jsx
@@ -1,7 +1,9 @@
 import { useSelector } from 'react-redux'
 import './styles/desktop.css'
+import './styles/mobile.css'
 import { GetInfo, GetSocials } from './request'
 import { Icon } from '@iconify/react/dist/iconify.js'
+
 
 function BottomBar() {
     const sesion = useSelector((state) => state.sesion).name
@@ -10,6 +12,7 @@ function BottomBar() {
         // <div className={`bottomBar ${sesion === 'about' ? 'fixed' : ''}`}>
         <div className={`bottomBar`}>
             <ContactInfo />
+            <SendEmail />
         </div>
     )
 }
@@ -39,7 +42,31 @@ function ContactInfo() {
 
 function SendEmail() {
     return (
-        <div>
+        <div className='emailZone'>
+            <h4>ESCRIBENOS</h4>
+            <div >
+                <div className='nameEmail'>
+                    <div>
+                        <text>Escribe tu nombre</text>
+                        <input className='input' type="text" placeholder='nombre' />
+                    </div>
+
+                    <div>
+                        <text>Escribe tu nombre</text>
+                        <input className='input' type="text" placeholder='email' />
+                    </div>
+                </div>
+
+                <div className='nameEmail'>
+                    <div>
+                        <text style={{ opacity: 0.7 }}>Escribe tu mensaje</text>
+                        <textarea className='input area' type="text" />
+                    </div>
+                    <button className='sendButton'>
+                        Enviar
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
